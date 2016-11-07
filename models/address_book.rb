8fa025e98624c36289587a44b2d63787e959fc1e
Yuriy Ivanov <ymivanov@icloud.com>
2016-11-06 19:20:08 -0500

    Checkpoint-7-Complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,5 @@
 require_relative "entry"
+require "csv"
 
 class AddressBook
 
@@ -11,7 +12,7 @@ class AddressBook
 	def add_entry(name, phone_number, email)
 
 		index = 0
-		entries.each do |entry| 
+		entries.each do |entry|
 			if name < entry.name
 				break
 			end
@@ -21,5 +22,14 @@ class AddressBook
 		entries.insert(index, Entry.new(name, phone_number, email))
 	end
 
-end
+	def import_from_csv(file_name)
 
+		csv_text = File.read(file_name)
+		csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+
+		csv.each do |row|
+			row_hash = row.to_hash
+			add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+		end
+	end
+end
